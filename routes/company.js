4a9074434beb207d387994eb563b6f056b834b64
faizal <faizalramanda@gmail.com>
2018-03-28 23:11:08 +0700

    tambah views dan routes company

--- routes/company.js
+++ routes/company.js
@@ -1,10 +1,68 @@
 const express = require('express')
 const router = express.Router();
+const db = require('../models');
 
+//edit profile, add/delete vacancies, view applicants, change status
 
-router.get('/', function(req, res){
-  res.render('company/index');
+router.get('/', (req, res) => {
+  // db.Job_vacancy.findAll({
+  //   order: ['id'],
+  //   include: [{
+  //     model: db.Company,
+  //     as: 'Company' // specifies how we want to be able to access our joined rows on the returned data
+  //   }]
+  // })
+  // .then(jobs =>{
+  //   db.Company.findAll()
+  //   .then(vacancies => {
+  //     res.render('company/index', {jobs: jobs, vacancies: vacancies})
+  //   })
+  // })
+  // .catch(console.error)
 })
+
+router.get('/addvacancy/:id', (req, res) => {
+  res.render('company/vacancyform')
+})
+router.post('/addvacancy/:id', (req, res) => {
+  db.Item.create({
+    name: req.body.name,
+    field_of_work: req.body.field_of_work,
+    required_gender: req.body.required_gender,
+    required_age: req.body.required_age,
+    required_education: req.body.required_education,
+    required_experience: req.body.required_experience,
+    salary: req.body.salary,
+    status: 'open',
+    CompanyId: req.params.id
+  }).then( itemdata => {
+    res.redirect('/')
+  })
+})
+
+router.get('/:id/edit', (req, res) => {
+  db.Company.findById(req.params.id)
+    .then(company => {
+      res.render('company_form', { company: company })
+    })
+})
+
+router.post('/:id/edit', (req, res) => {
+  db.Company.update({
+    name: req.body.name,
+    location: req.body.location,
+    email: req.body.email,
+    website: req.body.website,
+    phone: req.body.phone
+  },{
+    where: { id: req.params.id }
+  }).then(
+    // (companyUpdated) => {
+  res.redirect('/company')
+  //}
+)
+})
+
 
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,31 @@
 const express = require('express')
 const router = express.Router()
+const db = require('../models');
 
 router.get('/', function(req, res){
   res.render('index');
+})
+
+router.post('/login', function(req, res){
+  if (req.body.option === 'company') {
+
+  } else {
+
+  }
+})
+
+router.post('/register', function(req, res){
+  if (req.body.option === 'company') {
+    db.Company.create({
+      email: req.body.email,
+      password: req.body.password
+    }).then( company => {
+      res.render('company_form', { company: company })
+    })
+    // res.render('company_form', {company: req.body})
+  } else {
+    res.render('candidate_form')
+  }
 })
 
 module.exports = router;

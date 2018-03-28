c0fb9d5f3c539a76e7555003acde17a20aebe78a
Dani Damara <danny12march@gmail.com>
2018-03-29 02:32:55 +0700

    change app.use session resave to false

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(bodyParser.urlencoded({extended:false}))
 app.use(cookieParser());
 app.use(session({
   secret: 'keyboard cat',
-  resave: true,
+  resave: false,
   saveUninitialized: true
 }))
 

41e5dc7ae461621505e9b24d57406e017021373e
Dani Damara <danny12march@gmail.com>
2018-03-28 15:47:00 +0700

    add migrations models and seeders

---
+++ migrations/20180328083821-create-candidate.js
@@ -0,0 +1,60 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Candidates', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      gender: {
+        type: Sequelize.STRING
+      },
+      age: {
+        type: Sequelize.INTEGER
+      },
+      location: {
+        type: Sequelize.STRING
+      },
+      email: {
+        type: Sequelize.STRING
+      },
+      phone: {
+        type: Sequelize.STRING
+      },
+      expected_sallary: {
+        type: Sequelize.INTEGER
+      },
+      experience: {
+        type: Sequelize.STRING
+      },
+      field_of_work: {
+        type: Sequelize.STRING
+      },
+      education: {
+        type: Sequelize.STRING
+      },
+      password: {
+        type: Sequelize.STRING
+      },
+      salt: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Candidates');
+  }
+};

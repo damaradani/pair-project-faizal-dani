4aef1c0bfdf3c4ea5a76d064b5514aec96c3708d
Dani Damara <danny12march@gmail.com>
2018-03-28 23:13:07 +0700

    Create View Job Vacancy in candidates

--- routes/candidates.js
+++ routes/candidates.js
@@ -1,9 +1,20 @@
 const express = require('express')
 const router = express.Router();
+const db = require('../models/');
 
 //halaman utama Candidates
 router.get('/', function(req, res){
-  res.render('candidates/index')
+  db.Job_vacancy.findAll(
+        {include:[db.Company]}
+      )
+    .then(Jobs =>{
+      //console.log(JSON.stringify(x, null, 2));
+      res.render('candidates/index', {Jobs})
+    })
+    .catch(err =>{
+      console.log(err);
+    })
+
 })
 
 //halaman Edit Profile
@@ -15,6 +26,20 @@ router.post('/edit', function(req, res){
   // res.render('candidates/editProfile')
 })
 
+//halaman apply Jobs
+router.get('/apply/:Jobsid', function(req, res){
+  //id candidates dari seesion rencanannya
+    // let candidates_Jobs ={
+    //   CandidateId: req.session.something,
+    //   JobVacancyId: req.params.id
+    // }
+  //yang status nanti di bikin di hooks beforeCreate isinya sesuai dari
+  //
+  //nanti masuk ke halaman dimana isinya tabel daftar
+  //Jobs yang sudah di apply
+
+  res.render('candidates/apply', )
+})
 
 
 module.exports = router;
